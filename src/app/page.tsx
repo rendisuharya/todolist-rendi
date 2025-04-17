de34732bdfd897ce5a6bedc590d03e63c37d732b
rendisuharya <rendisuharya05@gmail.com>
2025-04-17 20:41:55 +0700

    import todolist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import TodoList from './components/todolist';
 
 function page() {
   return (
     <>
-      <h1>Landing Page </h1>
+      <TodoList/>
     </>
   );
 }
